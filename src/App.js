972e9d3051bde252dcced7d642dfea8194ed8175
nivasbn <nivasniva97@gmail.com>
2022-11-14 14:02:10 +0800

    button added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Buttons from "./components/Buttons"
 
 function App() {
   return (
     <div className="App">
     <h1>test basic</h1>
+    <Buttons/>
     </div>
   );
 }
