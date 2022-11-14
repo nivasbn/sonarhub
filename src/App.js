d57dcea189ff7f65a5aca91e84e8fab0f2998d4e
nivasbn <nivasniva97@gmail.com>
2022-11-14 14:49:20 +0800

    latest edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Buttons from "./components/Buttons"
 
 function App() {
   return (
     <div className="App">
     <h1>test basic</h1>
-    <Buttons/>
+ 
     </div>
   );
 }
